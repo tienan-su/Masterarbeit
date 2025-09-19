95171dc9cfde5f07f256629da0878aa19e2d2ad5
Tien An <ane10su@gmail.com>
2025-09-19 16:51:15 +0200

    Update wind_trainer.py

--- wind_trainer.py
+++ wind_trainer.py
@@ -77,7 +77,103 @@ class WindTrain(ABC):
 
 ###################################################
 ###################################################
+"""
+Train_inst = Trainer(
+    model_str,
+    num_epochs=num_epochs,
+    optim_adam=optim_adam,
+    scheduler=scheduler,
+    loss_func = wind_loss_func
+)
+"""
 
+"""
+class Trainer(WindTrain):
+    def __init__(
+        self,
+        model_str,
+        optim_adam,
+        scheduler,
+        wind_loss_func,
+        num_epochs=1500,
+    ):
+        """
+        # Usage
+        # Define your train loaders, features_calc_AC, calculate_theta_AC, loss_func_AC, etc.
+        # Create optimizer and scheduler objects
+        # Instantiate the EnsembleTrainer class
+        # Call the train method on the instance
+        
+        # Example Usage:
+        # ensemble_trainer = EnsembleTrainer(model_str, num_epochs, optim_adam, scheduler)
+        # ensemble_trainer.train(train_loader, features_calc_AC, calculate_theta_AC, loss_func_AC)
+        """
+        
+        super().__init__()
+        self.model_str = model_str
+        self.num_epochs = num_epochs
+        self.optim_adam = optim_adam
+        self.scheduler = scheduler
+        self.wind_loss_func = wind_loss_func
+
+        self.loss_total = []
+        self.coef_s = []
+        
+
+    def train_func(
+        self,
+        train_loader,
+    ):
+        #loop = tqdm(enumerate(train_loader), total=len(train_loader), leave=False)
+        
+        loop = tqdm(range(self.num_epochs), leave=False)
+        
+        for epoch in loop:
+            #tqdm.write(f"Epoch: {epoch}")
+
+            loss_data = 0
+            start_time = time.time()
+
+            ####################################################
+            wind_loss_instance = WindLoopProcessor(
+                self.model_str, self.wind_loss_func
+            )
+            
+            
+            loss_data= wind_loss_instance(train_loader)
+          
+
+            ####################################################
+            loss = loss_data
+            self.loss_total.append(loss.cpu().detach().numpy())
+            self.optim_adam.zero_grad()
+            loss.backward()
+            self.optim_adam.step()
+
+            # scheduler step
+            self.scheduler.step()
+            
+           
+            
+            #loop.set_description(f"Epoch [{epoch}/{self.num_epochs}]")
+            loop.set_postfix(
+                training_loss=loss.item(),)
+            
+
+        self.loss_total = np.array(self.loss_total)
+        
+
+        loss_func_list = [
+            self.loss_total,
+            
+        ]
+       
+
+        return loss_func_list
+"""
+
+###################################################
+###################################################
 
 class LSTMTrainer(WindTrain):
     def __init__(
